da1727680c420f0fd3dba7941728460dcc943271
tristandefournas <tristan.de-fournas@cpe.fr>
2015-11-02 16:26:54 +0100

    JE PEUX PAS TESTER PARCE QUE J4AI PAS DE JOUEUR

--- src/launcher/localLauncher/LauncherGUI.java
+++ src/launcher/localLauncher/LauncherGUI.java
@@ -1,5 +1,27 @@
 package launcher.localLauncher;
 
-public class LauncherGUI {
+import javax.swing.JFrame;
 
+
+import controler.controlerLocal.ChessGameControler;
+import model.observable.ChessGame;
+import vue.ChessGameGUI;
+
+public class LauncherGUI {
+	
+	public static void main(String[] args) {		
+		
+		ChessGame chessGame;
+		ChessGameControler chessGameControler;		
+		
+		chessGame = new ChessGame();	
+		chessGameControler = new ChessGameControler(chessGame);
+		
+		JFrame frame = new ChessGameGUI(chessGameControler);	
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setResizable(true);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 }
