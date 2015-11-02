c17ef82bcdca4ec5f4904392948b32a400b229dc
tristandefournas <tristan.de-fournas@cpe.fr>
2015-11-02 13:42:00 +0100

    pouet

--- controler/controlerLocal/ChessGameControler.java
+++ controler/controlerLocal/ChessGameControler.java
@@ -1,5 +1,7 @@
 package controler.controlerLocal;
 
 public class ChessGameControler {
+	
+	
 
 }
--- model/observable/ChessGame.java
+++ model/observable/ChessGame.java
@@ -1,5 +1,55 @@
 package model.observable;
 
-public class ChessGame {
+import com.sun.prism.paint.Color;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import model.Couleur;
+import model.Echiquier;
+
+public class ChessGame implements Observable{
+	
+	public Echiquier echiquier; 
+	
+	public ChessGame(){
+		echiquier = new Echiquier();
+	}
+	
+	public String toString(){
+		String retour = "";
+		retour += echiquier.toString();
+		retour += "\n";
+		retour += "Déplacement de " + /*position initiale +*/ " vers " + /*position finale +*/ " : " + /*position statut +*/ " : déplacement simple";
+		return retour;
+	}
+	
+	public boolean move (int xInit, int yInit, int xFinal, int yFinal){
+		
+	}
+	
+	public boolean isEnd(){
+		
+	}
+	
+	public String getMessage(){
+		
+	}
+	
+	public Couleur getColorCurrentPlayer(){
+		return ColorCurrentPlayer;
+	}
+
+
+	@Override
+	public void addListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
