9fe8d1fbb8c50f65c78461a91704ec059155b824
arca223 <arcanis223@gmail.com>
2015-11-02 13:40:22 +0100

    chessgame controler

--- controler/controlerLocal/ChessGameControler.java
+++ controler/controlerLocal/ChessGameControler.java
@@ -1,5 +1,40 @@
 package controler.controlerLocal;
 
-public class ChessGameControler {
+import model.*;
+import model.observable.*;
+
+public class ChessGameControler implements ChessGameControlers{
+	
+
+	private ChessGame chessGame;
+
+	public ChessGameControler() {
+		chessGame = new ChessGame(new Echiquier());
+	}
+
+	@Override
+	public boolean move(Coord initCoord, Coord finalCoord) {
+		return chessGame.move(initCoord.x, initCoord.y, finalCoord.x, finalCoord.y);
+	}
+
+	@Override
+	public String getMessage() {
+		return ChessGame.getMessage();
+	}
+
+	@Override
+	public boolean isEnd() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Couleur getColorCurrentPlayer() {
+		return chessGame.getColorCurrentPlayer();
+	}
+	
+	
+
+
 
 }
